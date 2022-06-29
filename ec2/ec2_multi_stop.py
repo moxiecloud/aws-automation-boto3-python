9820d89820435180d1ceb4f973a87d77aad1cac3
Xenix <Xenix@Xenix.local>
2022-06-28 20:30:55 -0400

    added more detailed comments

--- ec2/ec2_multi_stop.py
+++ ec2/ec2_multi_stop.py
@@ -4,24 +4,38 @@
 #
 # **Create a Python script that you can run that will stop all instances.**
 #
+# Boto3 session is an object to create a connection to your AWS service and
+# manage the connection state throughout your program life cycle.
+# You can create Boto3 session using your AWS credentials Access key id and secret access key.
+# The boto3 library is an AWS SDK for Python. It provides object-oriented API services and low-level
+# services to the AWS services. It allows users to create, and manage AWS services.
 
 import boto3
 
+# All API services are available in the Boto3 Client. Maps 1:1 with the AWS service API.
+# The client provides methods to connect with AWS services similar to the AWS API service.
+# Create a low-level client for the ec2 service
 ec2_client = boto3.client('ec2')
 
+# Use the describe_instances method to get a list of all running instances
 response = ec2_client.describe_instances()
+
 
 data = response["Reservations"]
 
+# initialize list
 ec2_list=[]
 
+# for loop to process the content under the Reservations list.
+# Will extract the Instance Name and Instance ID that will be sent to the
+# stop instance method.
 for instances in data:
     instance=instances["Instances"]
     for ids in instance:
         instance_id=ids["InstanceId"]
         ec2_list.append(instance_id)
 
-
+# list of instances that are running will be stopped by the stop_instances boto3 method.
 ec2_client.stop_instances(InstanceIds=ec2_list)
 
 print("all EC2 instances stopped")
